1b86dc8bb63afd4141cd3b7ae0fc58b0fa41bb81
aryan-kazemian <kazemianaryan@gmail.com>
2025-11-21 20:46:29 +0700

    fix: update DATABASES config to use dj_database_url properly with conn_max_age

--- InventoryHub/settings.py
+++ InventoryHub/settings.py
@@ -99,8 +99,8 @@ if os.getenv("CI") == "true":
 else:
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.environ.get("DATABASE_URL"),
-            engine="django.db.backends.postgresql"
+            engine="django.db.backends.postgresql",
+            conn_max_age=600
         )
     }
 
